d97133eb5629b917f889398ac321e3d233edebb5
Temur Murtazaev <temur.murtazaeveurope@gmail.com>
2023-09-18 23:17:58 +0200

    TeenNumberChecker

---
+++ src/main/java/com/example/javacrash/Methods/TeenNumberChecker.java
@@ -0,0 +1,28 @@
+package com.example.javacrash.Methods;
+
+public class TeenNumberChecker {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean hasTeen(int first, int second, int third){
+
+        if((first >= 13 && first <= 19) || (second >= 13 && second <= 19) || (third >= 13 && third <= 19)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public static boolean isTeen (int first){
+        if((first >= 13 && first <= 19)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+
+}
