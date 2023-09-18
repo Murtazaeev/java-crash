290fe41d70efdfb0612268fce176fece53c000f2
Temur Murtazaev <temur.murtazaeveurope@gmail.com>
2023-09-18 22:16:44 +0200

    ISDogBarking exercise

---
+++ src/main/java/com/example/javacrash/Methods/isDogBarking.java
@@ -0,0 +1,17 @@
+package com.example.javacrash.Methods;
+
+public class isDogBarking {
+    public static void main(String[] args) {
+        System.out.println( shouldWakeUp(true, -1));
+
+    }
+
+    public static boolean shouldWakeUp(boolean isBarking, int hourOfDay){
+        if(!isBarking || hourOfDay < 0 || hourOfDay > 23 || (hourOfDay > 7 && hourOfDay < 23)){
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+}
