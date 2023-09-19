1b47060052a55cc8beb94f9f360a9b16e4440863
Temur Murtazaev <temur.murtazaeveurope@gmail.com>
2023-09-20 01:00:20 +0200

    calculateInterest

---
+++ src/main/java/com/example/javacrash/ForLoop/ForLoop.java
@@ -0,0 +1,17 @@
+package com.example.javacrash.ForLoop;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        for(int counter = 1; counter <= 5; counter++){
+            System.out.println(counter);
+        }
+
+        for (double interestRate = 7.5; interestRate <= 10; interestRate += 0.25){
+            double calcInterest = calculateInterest(100, interestRate);
+            System.out.println("wdwdwd" + interestRate +" wdwdwdw" +  calcInterest);
+        }
+    }
+    public static double calculateInterest(double amount, double interestRate){
+        return (amount * (interestRate/100));
+    }
+}
