c56bd43743357476df7b5cf1bc445b561b3407d5
Temur Murtazaev <temur.murtazaeveurope@gmail.com>
2023-09-19 16:34:23 +0200

    printYearsAndDays

---
+++ src/main/java/com/example/javacrash/MethodOverloading/printYearsDaysCalculator.java
@@ -0,0 +1,19 @@
+package com.example.javacrash.MethodOverloading;
+
+public class printYearsDaysCalculator {
+    public static void main(String[] args) {
+        printYearsAndDays(525600);
+
+    }
+    public static void printYearsAndDays(long minutes){
+        long toYear = minutes / (60 * 24 * 365);
+        long leftYears = minutes % (60 * 24 * 365);
+        long toDays = leftYears / (24 * 60);
+        if(minutes < 0){
+            System.out.println("Invalid Value");
+        }
+        else {
+            System.out.println( minutes + " min = " + toYear +  " y and " + toDays + " d" );
+        }
+    }
+}
