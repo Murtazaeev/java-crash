36a3b2b6e0aaf16e950f611b43cbf15c86c1100c
Temur Murtazaev <temur.murtazaeveurope@gmail.com>
2023-09-20 20:43:45 +0200

    getEvenDigitSum

---
+++ src/main/java/com/example/javacrash/ChallangesForLoops/getEvenDigitSum.java
@@ -0,0 +1,22 @@
+package com.example.javacrash.ChallangesForLoops;
+
+public class getEvenDigitSum {
+
+    public static void main(String[] args) {
+        getEvenDigitSum(22222);
+    }
+
+    public static int getEvenDigitSum (int number){
+        if (number <0) return -1;
+
+        int sum = 0;
+        int remainder = 0;
+        while (number > 0){
+            remainder = number % 10;
+            sum += (remainder % 2 == 0) ? remainder:0;
+            number /= 10;
+        }
+        return sum;
+    }
+
+}
