6d88a3c72ea31d226a021477f4e1a0cbb6d02d4d
Temur Murtazaev <temur.murtazaeveurope@gmail.com>
2023-09-26 23:39:15 +0200

    BankAccount challange with getter and setter

---
+++ src/main/java/com/example/javacrash/ChallangesForLoops/largestPrime.java
@@ -0,0 +1,17 @@
+package com.example.javacrash.ChallangesForLoops;
+
+public class largestPrime {
+
+    public static int getLargestPrime(int number) {
+        if (number < 2) {
+            return -1;
+        }
+        for (int i = 2; i < number; i++) {
+            if ((number % i) == 0) {
+                number /= i;
+                i--;
+            }
+        }
+        return number;
+    }
+}
