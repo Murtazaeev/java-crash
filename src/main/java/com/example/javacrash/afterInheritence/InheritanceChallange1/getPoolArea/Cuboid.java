bec045b39c329b26b3c2c6cffbf3c7161c561863
Temur Murtazaev <temur.murtazaeveurope@gmail.com>
2023-10-03 22:37:44 +0200

    challange for getting area of pool

---
+++ src/main/java/com/example/javacrash/afterInheritence/InheritanceChallange1/getPoolArea/Cuboid.java
@@ -0,0 +1,20 @@
+package com.example.javacrash.afterInheritence.InheritanceChallange1.getPoolArea;
+
+public class Cuboid extends Rectangle{
+    private double height;
+
+
+    public Cuboid(double width, double length, double height) {
+        super(width, length);
+        if(height < 0) height = 0;
+        this.height = height;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getVolume() {
+        return (height * getArea());
+    }
+}
