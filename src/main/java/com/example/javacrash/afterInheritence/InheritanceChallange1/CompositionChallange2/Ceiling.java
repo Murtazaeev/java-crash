eea0eae663013a4c6b0db630fa915dbfe714ff57
Temur Murtazaev <temur.murtazaeveurope@gmail.com>
2023-10-04 07:04:25 +0200

    Bedroom challange / Composition

---
+++ src/main/java/com/example/javacrash/afterInheritence/InheritanceChallange1/CompositionChallange2/Ceiling.java
@@ -0,0 +1,21 @@
+package com.example.javacrash.afterInheritence.InheritanceChallange1.CompositionChallange2;
+
+public class Ceiling {
+
+    private int height;
+    private int paintedColor;
+
+    public Ceiling(int height, int paintedColor) {
+        this.height = height;
+        this.paintedColor = paintedColor;
+    }
+
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getPaintedColor() {
+        return paintedColor;
+    }
+}
